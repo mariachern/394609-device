a53f4b6f174cc51c27cc67e43aa2192a7efd3321
Maria Chern <mariachern@users.noreply.github.com>
2017-12-08 23:51:09 +0300

    Итог

--- js/main.js
+++ js/main.js
@@ -5,29 +5,70 @@ var form = popup.querySelector("form");
 var userName = popup.querySelector("[name=user-name]");
 var userEmail = popup.querySelector("[name=user-email]");
 var userMessage = popup.querySelector("[name=user-message]");
+
+var storageUserName = localStorage.getItem("userName");
+var storageUserEmail = localStorage.getItem("userEmail");
+
+console.log(localStorage.getItem("userName"));
+console.log(localStorage.getItem("userEmail"));
   
 open.addEventListener("click", function(evt) {
     evt.preventDefault();
     popup.classList.add("modal-show");
-    userName.focus();
+    if (storageUserName && storageUserEmail) {
+        userName.value = storageUserName;
+        userEmail.value = storageUserEmail;
+        userMessage.focus(); 
+    } else if (storageUserName) {
+        userName.value = storageUserName;
+        userEmail.focus();
+    } else {
+        userName.focus();
+    }
+  });
+
+form.addEventListener("submit", function(evt) {
+    if (!userName.value) {
+        evt.preventDefault();
+        userName.classList.add("invalid-item");
+    }
+    else {
+      localStorage.setItem("userName", userName.value);
+    }
+  });
+
+form.addEventListener("submit", function(evt) {
+    if (!userEmail.value) {
+        evt.preventDefault();
+        userEmail.classList.add("invalid-item");
+    }
+    else {
+      localStorage.setItem("userEmail", userEmail.value);
+    }
+  });
+
+form.addEventListener("submit", function(evt) {
+    if (!userMessage.value) {
+        evt.preventDefault();
+        userMessage.classList.add("invalid-item");
+    }
   });
 
 close.addEventListener("click", function(evt) {
     evt.preventDefault();
     popup.classList.remove("modal-show");
-  });
-
-form.addEventListener("submit", function(evt) {
-    if (!userName.value || !userEmail.value || !userMessage) {
-      evt.preventDefault();
-      alert("Все поля должны быть заполнены");
-    }
+    userName.classList.remove("invalid-item");
+    userEmail.classList.remove("invalid-item");
+    userMessage.classList.remove("invalid-item");
   });
 
 window.addEventListener("keydown", function(evt) {
     if (evt.keyCode === 27) {
         if (popup.classList.contains("modal-show")) {
             popup.classList.remove("modal-show");
+            userName.classList.remove("invalid-item");
+            userEmail.classList.remove("invalid-item");
+            userMessage.classList.remove("invalid-item");
         }
     }
 });
